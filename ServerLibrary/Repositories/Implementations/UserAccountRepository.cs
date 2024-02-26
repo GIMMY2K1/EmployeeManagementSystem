cacaf7f876366b0a17c91712793a0b2907bb5808
Gimmy <gimmy.susan@ateikon.net>
2024-02-26 15:48:12 +0100

    wip: 21:08 creata action CreateAsync

--- ServerLibrary/Repositories/Implementations/UserAccountRepository.cs
+++ ServerLibrary/Repositories/Implementations/UserAccountRepository.cs
@@ -1,18 +1,84 @@
 ﻿using BaseLibrary.DTOs;
+using BaseLibrary.Entities;
 using BaseLibrary.Responses;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using ServerLibrary.Data;
+using ServerLibrary.Helpers;
 using ServerLibrary.Repositories.Contracts;
+using Constants = ServerLibrary.Helpers.Constants;
 
 namespace ServerLibrary.Repositories.Implementations;
 
-public class UserAccountRepository : IUserAccount
+public class UserAccountRepository(IOptions<JwtSection> config, AppDbContext appDbContext) : IUserAccount
 {
-    public Task<GeneralResponse> CreateAsync(Register user)
+    public async Task<GeneralResponse> CreateAsync(Register user)
     {
-        throw new NotImplementedException();
+        if (user is null) return new GeneralResponse(false, "Model is empty");
+
+        var checkUser = await FindUserByEmail(user.Email);
+        if (checkUser != null) return new GeneralResponse(false, "User registered already");
+
+        var applicationUser = await AddToDatabase(new ApplicationUser()
+        {
+            Id = Guid.NewGuid().ToString(),
+            FullName = user.FullName,
+            Email = user.Email,
+            Password = BCrypt.Net.BCrypt.HashPassword(user.Password),
+        });
+
+        //check, create and assign role
+        var checkAdminRole = await appDbContext.SystemRoles.FirstOrDefaultAsync(_ => _.Name!.Equals(Constants.Admin));
+        if (checkAdminRole is null)
+        {
+            var createAdminRole = await AddToDatabase(new SystemRole()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = Constants.Admin
+            });
+            return new GeneralResponse(true, "Account created!");
+        }
+
+        var checkUserRole = await appDbContext.SystemRoles.FirstOrDefaultAsync(_ => _.Name!.Equals(Constants.User));
+        SystemRole response = new();
+        if (checkUserRole is null)
+        {
+            response = await AddToDatabase(new SystemRole()
+            {
+                Id = Guid.NewGuid().ToString(),
+                Name = Constants.User
+            });
+            await AddToDatabase(new UserRole()
+            {
+                Id = Guid.NewGuid().ToString(),
+                RoleId = response.Id,
+                UserId = applicationUser.Id
+            });
+        }
+        else
+        {
+            await AddToDatabase(new UserRole()
+            {
+                Id = Guid.NewGuid().ToString(),
+                RoleId = checkUserRole.Id,
+                UserId = applicationUser.Id
+            });
+        }
+        return new GeneralResponse(true, "Account created!");
     }
 
     public Task<LoginResponse> SignInAsync(Login user)
     {
         throw new NotImplementedException();
+    }
+
+    private async Task<ApplicationUser> FindUserByEmail(string email) =>
+        await appDbContext.ApplicationUsers.FirstOrDefaultAsync(_ => _.Email!.ToLower()!.Equals(email!.ToLower()));
+
+    private async Task<T> AddToDatabase<T>(T model)
+    {
+        var result = appDbContext.Add(model!);
+        await appDbContext.SaveChangesAsync();
+        return (T)result.Entity;
     }
 }
