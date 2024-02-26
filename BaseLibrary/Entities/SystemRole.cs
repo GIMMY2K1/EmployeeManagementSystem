92125affe509eb01e3434da2acdd9b8aec0f7d3a
Gimmy <gimmy.susan@ateikon.net>
2024-02-26 14:55:34 +0100

    wip: 04:00 Aggiunte Entity UserRole, SystemRole

---
+++ BaseLibrary/Entities/SystemRole.cs
@@ -0,0 +1,7 @@
+﻿namespace BaseLibrary.Entities;
+
+public class SystemRole
+{
+    public string Id { get; set; }
+    public string? Name { get; set; }
+}
---
+++ BaseLibrary/Entities/UserRole.cs
@@ -0,0 +1,8 @@
+﻿namespace BaseLibrary.Entities;
+
+public class UserRole
+{
+    public string Id { get; set; }
+    public string RoleId { get; set; }
+    public string UserId { get; set; }
+}
