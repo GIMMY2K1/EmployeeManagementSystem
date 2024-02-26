2c5da2bf913b0b69d759e4f35347ba85e35cbfa1
Gimmy <gimmy.susan@ateikon.net>
2024-02-26 15:10:33 +0100

    wip: 10:10 gestito JwtSection

---
+++ ServerLibrary/Helpers/JwtSection.cs
@@ -0,0 +1,8 @@
+﻿namespace ServerLibrary.Helpers;
+
+public interface JwtSection
+{
+    public string? Key { get; set; }
+    public string? Issuer { get; set; }
+    public string? Audience { get; set; }
+}
